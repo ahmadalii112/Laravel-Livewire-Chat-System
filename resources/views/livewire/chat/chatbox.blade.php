4202f929f8b25da8932d1a94c4331337914e5781
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-02-26 02:31:42 +0500

    chat box header design

--- resources/views/livewire/chat/chatbox.blade.php
+++ resources/views/livewire/chat/chatbox.blade.php
@@ -1,12 +1,34 @@
 <div>
-     <div class="chatbox_header">
+    <div class="chatbox_header">
+        <div class="return">
+            <i class="bi bi-arrow-left"></i>
+        </div>
 
-     </div>
-     <div class="chatbox_body">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda nobis optio porro quibusdam reiciendis saepe, sunt suscipit veniam! Amet aperiam aspernatur dolore illum labore officiis porro possimus praesentium sit tempora!
+        <div class="img_container">
+            <img src="https://picsum.photos/id/231/200/300" alt="">
+        </div>
 
-     </div>
-     <div class="chatbox_footer">
-          footer
-     </div>
+        <div class="name">Ali</div>
+
+        <div class="info">
+            <div class="info_item">
+                <i class="bi bi-telephone-fill"></i>
+            </div>
+            <div class="info_item">
+                <i class="bi bi-image"></i>
+            </div>
+            <div class="info_item">
+                <i class="bi bi-info-circle-fill"></i>
+            </div>
+        </div>
+    </div>
+    <div class="chatbox_body">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda nobis optio porro quibusdam reiciendis
+        saepe, sunt suscipit veniam! Amet aperiam aspernatur dolore illum labore officiis porro possimus praesentium sit
+        tempora!
+
+    </div>
+    <div class="chatbox_footer">
+        footer
+    </div>
 </div>
