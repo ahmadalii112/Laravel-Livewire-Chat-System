e9d25ce8de54f810f7f1d51fc58c7f80989a9e36
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-02-26 00:07:52 +0500

    Create relationships

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Conversation extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function messages(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function sender(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
 }
--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    public function conversation(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Conversation::class);
+    }
+    public function sender(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+    public function receiver(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
 }
