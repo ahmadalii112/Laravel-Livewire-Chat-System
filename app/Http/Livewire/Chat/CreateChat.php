6517463638ad13d913f414416d2f69406218b82a
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-02-26 13:45:24 +0500

    create conversation

--- app/Http/Livewire/Chat/CreateChat.php
+++ app/Http/Livewire/Chat/CreateChat.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Livewire\Chat;
 
+use App\Models\Conversation;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class CreateChat extends Component
 {
+    public $message = "Hello, How are You";
+
+    public function checkConversation($receiverId)
+    {
+        $checkedConversation = Conversation::query()
+            ->where(['receiver_id' => auth()->id(), 'sender_id' => $receiverId])
+            ->orWhere(['receiver_id' => $receiverId, 'sender_id' => auth()->id()])
+            ->get();
+        # if conversation does not exist then create a conversation
+        if ($checkedConversation->isEmpty()) {
+            $conversation = Conversation::create(['receiver_id' => $receiverId, 'sender_id' => auth()->id(), 'last_time_message' => now()]);
+            # create a message using relationship
+            $conversation->messages()->create(['receiver_id' => $receiverId, 'sender_id' => auth()->id(), 'body' => $this->message]);
+            dd($conversation->load('messages')->toArray());
+        } else {
+
+        }
+    }
+
     public function render()
     {
-        return view('livewire.chat.create-chat');
+        return view('livewire.chat.create-chat', [
+            'users' => User::all()->except(Auth::id())
+        ]);
     }
 }
--- resources/views/livewire/chat/create-chat.blade.php
+++ resources/views/livewire/chat/create-chat.blade.php
@@ -1,3 +1,9 @@
 <div>
-     create chat
+     <ul class="list-group w-75 mx-auto mt-3 container-fluid">
+          @forelse($users as $user)
+          <li wire:click="checkConversation({{ $user->id }})" class="list-group-item list-group-item-action"> {{ $user->name }}</li>
+          @empty
+               <h2>No Users found ☹</h2>
+          @endforelse
+     </ul>
 </div>
